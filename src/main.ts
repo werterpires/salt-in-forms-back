fe731145e953e2487b66f98f57397741fa773274
Werter Pires <werterpires23@hotmail.com>
2025-04-05 21:26:36 -0300

    Add custom logger service to write logs to file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { CustomLoggerService } from './shared/custom-logger/custom-logger.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomLoggerService()
+  })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
