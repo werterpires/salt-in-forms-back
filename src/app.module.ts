00e1b3faf1f18e0af1d24009af1ef472ab5a4435
Werter Pires <werterpires23@hotmail.com>
2025-04-09 10:05:29 -0300

    adding knex

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common'
 import { UtilsModuleModule } from './shared/utils-module/utils-module.module'
 import { APP_FILTER, APP_GUARD } from '@nestjs/core'
 import { GlobalErrorsFilter } from './shared/custom-error-handler/global-errors.filter'
 import { CustomErrorHandlerService } from './shared/custom-error-handler/custom-error-handler.service'
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { KnexModule } from 'nest-knexjs'
+import { config } from 'dotenv'
+
+config()
 
 const throttler = ThrottlerModule.forRoot({
   throttlers: [
@@ -14,8 +21,43 @@ const throttler = ThrottlerModule.forRoot({
   ]
 })
 
+const knex = KnexModule.forRoot({
+  config: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.SQL_HOST,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASS,
+      database: process.env.SQL_DB,
+      port: 3306,
+      typeCast: function (field, next) {
+        if (field.type === 'TINY' && field.length === 1) {
+          // retorna tipo booleano ou null
+          switch (field.string()) {
+            case null:
+            case undefined:
+            case '':
+            case 'null':
+            case 'NULL':
+              return null
+            case '0':
+              return false
+            case '1':
+              return true
+          }
+        } else if (field.type === 'DATE' && field.length > 1) {
+          return field.string() // 1 = true, 0 = false
+        } else if (field.type === 'DATETIME' && field.length > 1) {
+          return field.string().substring(0, 10) // 1 = true, 0 = false
+        }
+        return next()
+      }
+    }
+  }
+})
+
 @Module({
-  imports: [UtilsModuleModule, throttler],
+  imports: [UtilsModuleModule, throttler, knex],
   controllers: [],
   providers: [
     CustomErrorHandlerService,
